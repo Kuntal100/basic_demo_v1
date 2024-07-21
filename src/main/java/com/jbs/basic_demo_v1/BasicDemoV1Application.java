4799217832581efd2c328995acee6c7d54a3ea59
KUNTAL <KUNTAL@DESKTOP-0FPPNGB>
2024-07-21 13:00:43 +0530

    Second commit added

--- src/main/java/com/jbs/basic_demo_v1/BasicDemoV1Application.java
+++ src/main/java/com/jbs/basic_demo_v1/BasicDemoV1Application.java
@@ -9,6 +9,7 @@ public class BasicDemoV1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(BasicDemoV1Application.class, args);
 		System.out.println("hiii");
+		System.out.println("Application stated...");
 	}
 
 }
